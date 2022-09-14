411a4a4fe9f9c0d9370939d26e8a2c3f4bccfbbd
박세희 <park6938@naver.com>
2022-09-14 23:11:38 +0000

    0920

--- Algorithm/SWEA/5주차/십육진수의_십진수_출력.py
+++ Algorithm/SWEA/5주차/십육진수의_십진수_출력.py
@@ -1,17 +1,16 @@
 T = int(input())
 for tc in range(1, T+1):
-    lst = list(map(int, input()))
+    dct = {'0':'0000', '1':'0001', '2':'0010', '3':'0011', '4':'0100', '5':'0100', '6':'0110', '7':'0111', '8':'1000', '9':'1001', 'A':'1010', 'B':'1011', 'C':'1100', 'D':'1101', 'E':'1110', 'F':'1111'}
+    st1 = input()
+    st2 = ''
+    for ch in st1:
+        st2 += dct[ch]
 
-    for i in range(lst):
-        if lst[i] == 'A':
-            lst[i] = 10
-        elif lst[i] == 'B':
-            lst[i] = 11
-        elif lst[i] == 'C':
-            lst[i] = 12
-        elif lst[i] == 'D':
-            lst[i] = 13
-        elif lst[i] == 'E':
-            lst[i] = 14
-        elif lst[i] == 'F':
-            lst[i] = 15
+    ans = []
+    n = 0
+    for i in range(len(st2)):
+        n = int(st2[i]) + n*2
+        if (i%7)==6 or i==len(st2)-1:
+            ans.append(n)
+            n = 0
+    print(f'#{tc}', *ans)
