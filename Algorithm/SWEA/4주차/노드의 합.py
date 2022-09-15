9ccfb38b49de6bdd72965bce85cb04752b2a223e
SeheeP1217 <parksehee6938@gmail.com>
2022-09-15 17:57:03 +0900

    0915

--- Algorithm/SWEA/4주차/노드의 합.py
+++ Algorithm/SWEA/4주차/노드의 합.py
@@ -13,3 +13,23 @@ for tc in range(1, T+1):
     arr = [list(map(int, input().split())) for _ in range(M)]
     print(f"#{tc}", end=' ')
     node_number()
+
+
+# def postord(n):
+#     if n <= N:
+#         if lst[n] == 0:  # 하위노드 계산이 필요
+#             return postord(n * 2) + postord(n * 2 + 1)
+#         else:
+#             return lst[n]
+#     return 0
+
+# T = int(input())
+# for test_case in range(1, T + 1):
+#     N, M, L = map(int, input().split())
+#     lst = [0] * (N + 1)
+#     for _ in range(M):
+#         n, v = map(int, input().split())
+#         lst[n] = v
+#
+#     ans = postord(L)
+#     print(f'#{test_case} {ans}')
