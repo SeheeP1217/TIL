779ef06236e8646230d67115bc070a764294f09a
박세희 <parksehee6938@gmail.com>
2022-09-17 21:48:19 +0000

    0922

--- Algorithm/SWEA/5주차/최대상금.py
+++ Algorithm/SWEA/5주차/최대상금.py
@@ -1,5 +1,25 @@
+def change(idx, cnt):
+    global result
+    if cnt == int(c):
+        result = max(int(''.join(num)), result)
+        return
+    for i in range(idx, len(num)):
+        for max_idx in range(i + 1, len(num)):
+            if num[i] <= num[max_idx]:
+                num[i], num[max_idx] = num[max_idx], num[i]
+                change(i, cnt + 1)
+                num[i], num[max_idx] = num[max_idx], num[i]
+    if result == 0 and cnt < int(c):
+        rest = (int(c) - cnt) % 2
+        if rest == 1:
+            num[-1], num[-2] = num[-2], num[-1]
+        change(idx, int(c))
+
 T = int(input())
 for tc in range(1, T+1):
-    num, c = map(int, input().split())
+    num, c = input().split()
 
-    
+    num = list(num)
+    result = 0
+    change(0, 0)
+    print(f"#{tc} {result}")
