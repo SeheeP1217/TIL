36474db73aa7600e81a8b89476e1c870b32d36dd
sehee <parksehee6938@gmail.com>
2023-01-09 17:25:24 +0900

    0109 react

---
+++ REACT/summary/react-course/src/App.js
@@ -0,0 +1,14 @@
+import Todo from "./components/Todo.js"
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo />
+      <Todo />
+      <Todo />
+    </div>
+  )
+}
+
+export default App
