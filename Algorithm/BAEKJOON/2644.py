fcb962abfb49d3f56a5c013df4be971a5953a3a7
박세희 <park6938@naver.com>
2023-09-08 23:45:16 +0900

    0908 solved

---
+++ Algorithm/BAEKJOON/2644.py
@@ -0,0 +1,36 @@
+"""
+촌수계산
+DFS
+"""
+
+import sys
+
+def dfs(k, num):
+    num += 1
+    visited[k] = 1
+
+    if k == e:
+        result.append(num)
+
+    for j in graph[k]:
+        if visited[j]==0:
+            dfs(j, num)
+
+n = int(sys.stdin.readline())
+s, e = map(int, sys.stdin.readline().split())
+k = int(sys.stdin.readline())
+
+graph = [[] for i in range(n+1)]
+visited=[0]*(n+1)
+result = []
+
+for i in range(k):
+  a, b = map(int, sys.stdin.readline().split())
+  graph[a]+=[b]
+  graph[b]+=[a]
+
+dfs(s, 0)
+if len(result) == 0:
+    print(-1)
+else:
+    print(result[0]-1)
